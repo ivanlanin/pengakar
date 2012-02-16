4de9ee3c493cbb856ce99b827d4e2b80995eb886
Ivan Lanin <ivan@lanin.org>
2012-02-16 10:00:52 +0700

    Tidying

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if ($q) $ret .= $pengakar->stem($q);
 <p>Silakan masukkan kata (bisa dipisahkan dengan spasi, koma, atau baris baru) atau salin rekatkan teks pada kotak di bawah ini. Klik "Proses" dan pengakar akan berupaya mencari akar kata tersebut.</p>
 <?php echo($ret); ?>
 <hr />
-Lisensi: <a href="http://www.gnu.org/licenses/gpl.html">GPL</a> (Kode), <a href="http://creativecommons.org/licenses/by-nc/3.0/">CC-BY-NC</a> (<a href="./kamus.txt">Leksikon</a>) | <a href="./README.TXT">README.TXT</a>
+Lisensi: <a href="http://www.gnu.org/licenses/gpl.html">GPL</a> (<a href="https://github.com/ivanlanin/pengakar">Kode</a>), <a href="http://creativecommons.org/licenses/by-nc/3.0/">CC-BY-NC</a> (<a href="./kamus.txt">Leksikon</a>) | <a href="./README.TXT">README.TXT</a>
 </body>
 </html>
 <?php
@@ -81,6 +81,7 @@ class pengakar
 		foreach ($words as $word)
 		{
 			$tmp = $this->stem_word($word);
+			// FIXME: Stupid hack :P
 			if ($tmp)
 			{
 				if (substr($tmp, 0, 5) == '<span')
@@ -102,96 +103,111 @@ class pengakar
 	}
 
 	/**
-	 *
+	 * Stem individual word
 	 */
 	function stem_word($word)
 	{
-		$word = trim($word);
+		$url_template = 'http://kateglo.bahtera.org/?mod=dict&action=view&phrase=%1$s';
 
+		// Preprocess: Original word
+		$word = trim($word);
 		$roots = array($word => '');
 		if (in_array($word, $this->dict))
-			$roots[$word] = array('');
+			$roots[$word]['affixes'] = array();
 
-		$this->find_suffix($roots);
-		$this->find_prefix($roots);
-		$this->find_prefix($roots);
-		$this->find_prefix($roots);
+		// Process: Find suffixes, pronoun prefix, and other prefix (3 times, Asian)
+		$this->find_affixes($roots);
+		for ($i = 0; $i < 3; $i++)
+			$this->find_prefix($roots);
 
+		// Postprocess, remove when root not found in dictionary
 		$i = 0;
-		foreach ($roots as $lemma => $affixes)
+		foreach ($roots as $lemma => $attrib)
 		{
-			if (in_array($lemma, $this->dict))
+			if (!in_array($lemma, $this->dict))
+				unset($roots[$lemma]);
+			else
 			{
-				$i++;
-				$found = true;
-				unset($prefixes);
-				unset($suffixes);
-				$components .= $components ? '; ' : '';
-				if (is_array($affixes))
+				// Divide affixes into suffixes and prefixes
+				// Reverse suffix order
+				foreach ($attrib['affixes'] as $affix)
 				{
-					if ($i > 1) $components .= "({$i}) ";
-					foreach ($affixes as $affix)
-					{
-						if (substr($affix, 0, 1) == '-')
-							$suffixes[] = $affix;
-						else
-							$prefixes[] = $affix;
-					}
-					if (is_array($prefixes))
-					{
-						$components .= implode('', $prefixes);
-					}
-					$components .= sprintf('<a href="http://kateglo.bahtera.org/?mod=dict&action=view&phrase=%1$s">%1$s</a>', $lemma);
-					if (is_array($suffixes))
-					{
-						krsort($suffixes); // reverse suffix order
-						$components .= implode('', $suffixes);
-					}
+					$type = (substr($affix, 0, 1) == '-') ? 'suffixes' : 'prefixes';
+					$attrib[$type][] = $affix;
 				}
+				if (is_array($attrib['suffixes']))
+					krsort($attrib['suffixes']);
+				$roots[$lemma] = $attrib;
 			}
 		}
 
-		// final touch
-		if ($found)
+		// Return if no root match dictionary
+		// FIXME: HTML renderer should be handled by other function
+		$root_count = count($roots);
+		if ($root_count == 0)
+			return('<span style="color: #f00">' . $word .  '</span>');
+		else
 		{
-			if ($i > 1) $components = "(1) " . $components;
-			$url = sprintf('<a href="http://kateglo.bahtera.org/?mod=dict&action=view&phrase=%1$s">%1$s</a>', $word);
-			if ($url != $components)
-				$components = $word . ': ' . $components;
-			else
-				$components = $url;
+			$i = 0; unset($components);
+			foreach ($roots as $lemma => $attrib)
+			{
+				$i++;
+				$affixes = $attrib['affixes'];
+				$url = sprintf($url_template, $lemma);
+				$lemma_url = sprintf('<a href="%s">%s</a>', $url, $lemma);
+				$components .= $components ? '; ' : '';
+
+				// Return word with link only when baseword, else ...
+				if ($word == $lemma && $root_count == 1)
+					$components .= $lemma_url;
+				else
+				{
+					if ($i == 1) $components .= $word . ': ';
+					if ($root_count > 1) $components .= "({$i}) ";
+					if (is_array($attrib['prefixes']))
+						$components .= implode('', $attrib['prefixes']);
+					$components .= $lemma_url;
+					if (is_array($attrib['suffixes']))
+						$components .= implode('', $attrib['suffixes']);
+				}
+			}
 			return($components);
 		}
-		else
-			return('<span style="color: #f00">' . $word .  '</span>');
-
 	}
 
-	function find_suffix(&$roots)
+	/**
+	 * Find particle suffixes (part), pronoun suffixes (pros),
+	 * derivational suffixes (derv), and pronoun prefixes (proa)
+	 */
+	function find_affixes(&$roots)
 	{
-		$suffixes[1] = array('kah', 'lah', 'tah', 'pun');
-		$suffixes[2] = array('mu', 'ku', 'nya');
-		$suffixes[3] = array('i', 'kan', 'an');
-		foreach ($suffixes as $suffix_group)
+		$groups = array(
+			'part' => array('is_suffix' => 1, 'affixes' => 'kah,lah,tah,pun'),
+			'pros' => array('is_suffix' => 1, 'affixes' => 'mu,ku,nya'),
+			'derv' => array('is_suffix' => 1, 'affixes' => 'i,kan,an'),
+			'proa' => array('is_suffix' => 0, 'affixes' => 'ku,kau'),
+		);
+		foreach ($groups as $group)
 		{
-			foreach ($suffix_group as $suffix)
+			$affixes = explode(',', $group['affixes']);
+			$is_suffix = $group['is_suffix'];
+			foreach ($affixes as $affix)
 			{
-				$pattern = "/^(.+)({$suffix})$/i";
+				$pattern = $is_suffix ? "(.+)({$affix})" : "({$affix})(.+)";
+				$pattern = "/^{$pattern}$/i";
 				$this->add_root($roots, $pattern);
 			}
 		}
 	}
 
+	/**
+	 *
+	 */
 	function find_prefix(&$roots)
 	{
 		$_V = 'a|i|u|e|o'; // vowels
 		$_C = 'b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z'; // consonants
 		$_A = $_V . '|' . $_C; // any char
-
-		$pattern = "/^(ku)({$_A})(.+)$/i";
-		$this->add_root($roots, $pattern, '', 0);
-		$pattern = "/^(kau)({$_A})(.+)$/i";
-		$this->add_root($roots, $pattern, '', 0);
 
 		$rules = array(
 			array("(di)({$_A})(.+)", ""), // 0
@@ -200,7 +216,7 @@ class pengakar
 			array("(be)(r)({$_V})(.+)", ""), // 1
 			array("(ber)({$_A})(.+)", ""), // 1
 			array("(be)({$_C})({$_A})(er)(.+)", ""), // 3
-			array("(te)(r{$_V})(.+)", ""), // 6
+			array("(te)(r)({$_V})(.+)", ""), // 6
 			array("(ter)({$_A})(.+)", ""), // 6
 			array("(ter)({$_C})(er)({$_V})(.+)", ""), // 7
 			array("(ter)({$_C})({$_A})(.+)", ""), // 8
@@ -208,7 +224,7 @@ class pengakar
 			array("(mem)(b|f|v)(.+)", ""), // 11
 			array("(mem)({$_V})(.+)", "p"), // 13
 			array("(mem)(p)({$_C})(.+)", ""), // p + consonant: memproklamasikan
-			array("(mempe)(r{$_V})(.+)", ""), // 21
+			array("(mempe)(r)({$_V})(.+)", ""), // 21
 			array("(memper)({$_A})(.+)", ""), // 21
 			array("(mempel)({$_A})(.+)", ""), // 21
 			array("(men)(c|d|j|z)(.+)", ""), // 14
@@ -250,38 +266,46 @@ class pengakar
 	}
 
 	/**
-	 *
+	 * Greedy algorithm: add every possible branch
 	 */
 	function add_root(&$roots, $pattern, $variant = '', $is_suffix = 1)
 	{
-		foreach ($roots as $lemma => $affix)
+		foreach ($roots as $lemma => $attrib)
 		{
 			preg_match($pattern, $lemma, $matches);
 			if (count($matches) > 0)
 			{
 				unset($new_lemma); unset($new_affix);
 				$affix_index = $is_suffix ? 2 : 1;
+				$affixes = $attrib['affixes'];
+
 				// Lemma
 				for ($i = 1; $i < count($matches); $i++)
 					if ($i != $affix_index)
 						$new_lemma .= $matches[$i];
 				if ($variant)
 					$new_lemma = $variant . $new_lemma;
-				// Affix
+
+				// Affix, add - before (suffix), after (prefix)
 				$new_affix .= $is_suffix ? '-' : '';
 				$new_affix .= $matches[$affix_index];
 				$new_affix .= $is_suffix ? '' : '-';
-				// Only one instance allowed? seseorang?
-				//if (is_array($affix))
-				//	if (in_array($new_affix, $affix))
+
+				// Asian (2005): Only one instance allowed.
+				// Not valid for "seseorang": COMMENT OUT
+				//if (is_array($affixes))
+				//	if (in_array($new_affix, $affixes))
 				//		continue;
-				// Put into array and merge with other affixes
+
+				// Put into array and merge with existing affixes
 				$new_affix = array($new_affix);
-				if (is_array($affix))
-					$new_affix = array_merge($affix, $new_affix);
+				if (is_array($affixes))
+					$new_affix = array_merge($affixes, $new_affix);
+
 				// Push
-				$roots[$new_lemma] = $new_affix;
+				$roots[$new_lemma] = array('affixes' => $new_affix);
 			}
 		}
 	}
+
 }
